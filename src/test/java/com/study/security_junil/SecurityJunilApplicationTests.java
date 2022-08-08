a470e20df7dd76f09aa045d15012dea8b34023c7
code1218 <skjil1218@gmail.com>
2022-08-08 19:13:40 +0900

    시큐리티 시작

---
+++ src/test/java/com/study/security_junil/SecurityJunilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.security_junil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJunilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
